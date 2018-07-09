583e4a47c881fc19f022b643bcde9895966a8821
Rutger Luther <rluther11@yahoo.com>
2018-07-08 19:11:15 -0600

    Add files via upload

---
+++ legendzelda.py
@@ -0,0 +1,15 @@
+#LegendZelda
+
+for x in range(1,101):
+	if x %  21 == 0:
+		print("Legend of Zelda")
+		
+	elif x % 3 == 0:
+		print("Legend")
+		
+	elif x % 7 == 0:
+		print("Zelda")
+		
+	else:	
+		print(x)
+		
